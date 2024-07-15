47677f747e3e07ae71ba2abca15cb4569cbec2ce
aasawariMongoDB <103485554+aasawariMongoDB@users.noreply.github.com>
2024-07-16 01:20:50 +0530

    Undoing some code changes

--- src/test/java/com/tutorialCrud/repository/MongoDBSalesRepositoryTest.java
+++ src/test/java/com/tutorialCrud/repository/MongoDBSalesRepositoryTest.java
@@ -4,20 +4,16 @@ import com.tutorialCrud.model.Customer;
 import com.tutorialCrud.model.Item;
 import com.tutorialCrud.model.Sales;
 import org.bson.types.ObjectId;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
-
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -30,7 +26,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 public class MongoDBSalesRepositoryTest {
     @Container
-    @ServiceConnection
     public static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:7.0.0"));
 
     @Autowired
